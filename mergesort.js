9a961bf8cb5ab8e5a3fbc1888d5ba74b68299b98
Miapan <miapanyuewei@gmail.com>
2020-05-07 23:06:03 -0700

    mergesort

---
+++ mergesort.js
@@ -0,0 +1,14 @@
+//def merge(array, left, right)
+//n = right - left + 1
+//tmp = [ 0 for _ in range(n)]
+//mid = (left + right ) //2
+//i, j = ;eft, mid+1
+//for K in range(n)
+//if i <= mid and j > right or array[i] <= array[j]
+//tmp[k] =array [i]
+// i += 1
+//else 
+// tmp[K] = array[j]
+// i += 1
+// for K in rang(n)
+//array [left + K] =tmp[t]
